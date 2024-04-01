d7427e797dbb0369f415bed126ec64d64ad41e79
mariem <mariem.kanzari@esprit.tn>
2024-04-01 20:47:50 +0100

    commit mariem

---
+++ src/main/java/elderlycare/RestControllers/ChatController.java
@@ -0,0 +1,112 @@
+package elderlycare.RestControllers;
+
+import elderlycare.DAO.Entities.ChatMessage;
+import elderlycare.DAO.Entities.OurUsers;
+import elderlycare.Services.ChatMessageService;
+import elderlycare.Services.UserServiceM;
+import elderlycare.dto.ChatMessageRequest;
+import elderlycare.dto.VoiceMessageRequest;
+import jakarta.annotation.security.PermitAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/chat")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+@PermitAll
+public class ChatController {
+    private final ChatMessageService chatService;
+    private final UserServiceM userService;
+
+    @Autowired
+    public ChatController(ChatMessageService chatService, UserServiceM userService) {
+        this.chatService = chatService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/send-text-message")
+    public ResponseEntity<Object> sendMessage(@RequestBody ChatMessageRequest request) {
+        chatService.sendMessage(request.getSenderId(), request.getRecipientId(), request.getTextContent());
+        return ResponseEntity.ok().body(Map.of("message", "Message sent successfully"));
+    }
+
+
+    @PostMapping("/send-voice-message")
+    public ResponseEntity<Object> sendVoiceMessage(@RequestBody VoiceMessageRequest request) {
+
+            byte[] audioData = request.getAudioContent(); // Get audio data from request
+            chatService.saveVoiceMessage(request.getSenderId(), request.getRecipientId(), audioData);
+            return ResponseEntity.ok().body(Map.of("message", "Voice message saved successfully"));
+    }
+
+    // ChatController.java
+    @PutMapping("/update-message/{messageId}")
+    public ResponseEntity<Object> updateMessage(@PathVariable Long messageId, @RequestBody String updatedText) {
+        chatService.updateMessage(messageId, updatedText);
+        return ResponseEntity.ok().body(Map.of("message", "Message updated successfully"));
+    }
+
+
+    // ChatController.java
+    @DeleteMapping("/delete-message/{messageId}")
+    public ResponseEntity<Object> deleteMessage(@PathVariable Long messageId) {
+        chatService.deleteMessage(messageId);
+        return ResponseEntity.ok().body(Map.of("message", "Message deleted successfully"));
+    }
+
+
+
+
+    @GetMapping("/messages")
+    public ResponseEntity<List<ChatMessage>> getMessages(@RequestParam String userId1, @RequestParam String userId2) {
+        List<ChatMessage> messages = chatService.getMessages(userId1, userId2);
+        return ResponseEntity.ok(messages);
+    }
+
+
+    @GetMapping("/unseen-messages")
+    public ResponseEntity<List<ChatMessage>> getUnseenMessages(
+            @RequestParam("currentUserEmail") String currentUserEmail,
+            @RequestParam("senderEmail") String senderEmail) {
+
+        List<ChatMessage> unseenMessages = chatService.getUnseenMessages(currentUserEmail, senderEmail);
+        return ResponseEntity.ok(unseenMessages);
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<OurUsers>> getAllUsers() {
+        List<OurUsers> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+/*
+    //mariem
+    @GetMapping("/online-status")
+    public ResponseEntity<?> getOnlineStatus() {
+        try {
+            Map<String, Boolean> onlineStatus = userServiceM.getOnlineStatus();
+            return ResponseEntity.ok(onlineStatus);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching online status");
+        }
+    }
+
+    //mariem
+    @PutMapping("/update-online-status/{email}")
+    public void updateUserOnlineStatus(@PathVariable String email, @RequestBody boolean online) {
+        userServiceM.updateUserOnlineStatus(email, online);
+    }*/
+
+}
+
+
+
+
+
+
---
+++ src/main/java/elderlycare/Services/SchedulerServiceChat.java
@@ -0,0 +1,48 @@
+package elderlycare.Services;
+
+import elderlycare.DAO.Entities.Elderly;
+import elderlycare.DAO.Entities.Nurse;
+import elderlycare.DAO.Repositories.NurseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class SchedulerServiceChat {
+
+    @Autowired
+    private NurseRepository nurseRepository;
+
+    @Autowired
+    private ChatMessageService messageService;
+
+    @Transactional
+    public List<Nurse> getAllNursesWithElderly() {
+        return nurseRepository.findAll(); // Retrieve all nurses with their assigned elderly
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 0 8 * * *")
+    public void sendMorningMessages() {
+        List<Nurse> nurses = getAllNursesWithElderly();
+        for (Nurse nurse : nurses) {
+            for (Elderly elderly : nurse.getElderlys()) {
+                messageService.sendMorningMessageToElderly(nurse, elderly);
+            }
+        }
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 0 20 * * *")
+    public void sendEveningMessages() {
+        List<Nurse> nurses = getAllNursesWithElderly();
+        for (Nurse nurse : nurses) {
+            for (Elderly elderly : nurse.getElderlys()) {
+                messageService.sendEveningMessageToElderly(nurse, elderly);
+            }
+        }
+    }
+}
