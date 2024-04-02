8c5dd849a8d92868f5efe40e82ba8176824f91fb
ghofranetayari <ghofrane.tayari@esprit.tn>
2024-04-02 01:35:50 +0100

    MainMain5

---
+++ src/main/java/elderlycare/RestControllers/InfoController.java
@@ -0,0 +1,15 @@
+package elderlycare.RestControllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class InfoController {
+  /*  @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+
+    @GetMapping("/info")
+    public String getInfo() {
+        // Return information about the WebSocket connection
+        return "WebSocket connection information";
+    }*/
+}
